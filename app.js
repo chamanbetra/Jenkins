aee917a6cf601bedfaebd95990d8d2ea0d5f62b3
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-09-05 17:11:15 -0400

    added a second route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello, Jenkins CI/CD with Node.js!');
 });
 
+app.get('/route', (req, res) => {
+    res.send('Hello to the second route');
+})
+
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
 });
